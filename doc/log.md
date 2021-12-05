b216f7f2231995f7f0e2c2ed63f50d2cb570917a
yuexu-98 <jessicaxu0516@gmail.com>
2021-12-05 14:35:12 +0800

    epoll added

---
+++ doc/log.md
@@ -0,0 +1,15 @@
+
+### Install GCC on Ubuntu
+1. Update packages list: `apt update`
+2. Install build-essential: `apt install build-essential`
+3. Install Manual Pages: `apt-get install manpages-dev`
+
+### Install Git on Ubuntu
+1. Install Git: `apt-get install git`
+
+### Install OpenSSH
+1. Install: `apt-get install openssh-server`
+
+
+`# ./client`
+connect: Connection refused
--- src/epoll.c
+++ src/epoll.c
@@ -21,18 +21,69 @@ int main(){
     listen(lfd, 8);
 
 
-    int epfd = epoll_create(100);
+    // -- MARK 
 
-    struct spoll_event epev;
+    // 调用epoll_create()在内核创建一个epoll实例
+    int epfd = epoll_create(100);
+    
+
+    // 将监听的文件描述符相关的检测信息添加到epoll实例中
+    struct epoll_event epev;
     epev.events = EPOLL_IN;
     epev.data.fd = lfd;
-
     epoll_ctl(epfd, EPOLL_CTL_ADD, lfd, &epev)
 
+
+    struct epoll_event epevs[1024];
+    while(1){
+        int ret = epoll_wait(epfd, epevs, 1024, -1);
+        if(ret == -1){
+            perror("epoll_wait");
+            exit(-1);
+        }
+        printf("ret = %d\n", ret);
+
+        for(int i = 0; i < ret; i++){
+            int curfd = epevs[i].data.fd;
+
+            if (curfd == lfd){
+                // 有新的客户端连接；
+                struct sockaddr_in cliaddr;
+                // int len = sizepf(cliaddr);
+                // int cfd = accept(lfd, (struct sockaddr *)&cliaddr, &len);
+                socklen_t nAddrLen;
+                nAddrLen = sizeof(struct sockaddr_in);
+                int cfd = accept(lfd, (struct sockaddr *)&cliaddr, &nAddrLen);
+                
+                // 加入新的文件描述符
+                epev.events = EPOLL_IN;
+                epev.data.fd = cfd;
+                epoll_ctl(epfd, EPOLL_CTL_ADD, cfd, &epev)
+            }else{
+
+                // 有数据到达
+                char buf[1024] = {0};
+                int len = read(curfd, buf, sizeof(buf));
+
+                if (len == -1){
+                    perror("read");
+                    exit(-1);
+                }else if (len==0){
+                    printf("client closed ... \n");
+                    epoll_ctl(epfd, EPOLL_CTL_DEL, curfds,  NULL);
+                    close(curfd);
+                }else if (len > 0){
+                    printf("read buf = %s\n", buf);
+                    write(curfd, buf, strlen(buf)+1);
+                }
+            }
+        }
+    }
 
 
     // (4) 关闭
     close(lfd);
+    close(epfd);
 
     return 0;
 }
